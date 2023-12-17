1f1c6ebf3846b54ebc9a1d08c73828fe2ae1df33
Martin Norberg <nobbehhh@gmail.com>
2023-12-17 14:14:15 +0100

    solve with binary

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,51 +1,41 @@
 defmodule Day1 do
-  @regex ~r/(?=(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))/
-  @string_number_map %{
-    "1" => 1,
-    "2" => 2,
-    "3" => 3,
-    "4" => 4,
-    "5" => 5,
-    "6" => 6,
-    "7" => 7,
-    "8" => 8,
-    "9" => 9,
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "four" => 4,
-    "five" => 5,
-    "six" => 6,
-    "seven" => 7,
-    "eight" => 8,
-    "nine" => 9
-  }
+  def to_number(""), do: []
+  def to_number(<<c>> <> rest) when c in ?1..?9, do: [c - ?0 | to_number(rest)]
+  def to_number(<<_>> <> rest), do: to_number(rest)
 
-  def map_to_numbers(string) do
-    Regex.scan(@regex, string)
-    |> Enum.flat_map(& &1)
-    |> Enum.filter(&(&1 != ""))
-    |> Enum.map(fn
-      key when is_map_key(@string_number_map, key) -> @string_number_map[key]
-      key -> key
-    end)
-  end
+  def with_string_to_number(""), do: []
 
-  def solve_puzzle(string) do
-    string
-    |> Enum.map(fn c ->
-      case map_to_numbers(c) do
-        [] -> 0
-        list -> List.first(list) * 10 + List.last(list)
-      end
-    end)
+  def with_string_to_number(<<c>> <> rest) when c in ?1..?9,
+    do: [c - ?0 | with_string_to_number(rest)]
+
+  def with_string_to_number("one" <> rest), do: [1 | with_string_to_number("e" <> rest)]
+  def with_string_to_number("two" <> rest), do: [2 | with_string_to_number("o" <> rest)]
+  def with_string_to_number("three" <> rest), do: [3 | with_string_to_number("e" <> rest)]
+  def with_string_to_number("four" <> rest), do: [4 | with_string_to_number("r" <> rest)]
+  def with_string_to_number("five" <> rest), do: [5 | with_string_to_number("e" <> rest)]
+  def with_string_to_number("six" <> rest), do: [6 | with_string_to_number("x" <> rest)]
+  def with_string_to_number("seven" <> rest), do: [7 | with_string_to_number("n" <> rest)]
+  def with_string_to_number("eight" <> rest), do: [8 | with_string_to_number("t" <> rest)]
+  def with_string_to_number("nine" <> rest), do: [9 | with_string_to_number("e" <> rest)]
+  def with_string_to_number(<<_>> <> rest), do: with_string_to_number(rest)
+
+  def solve_puzzle(file, strategy) do
+    file
+    |> String.split("\n", trim: true)
+    |> Enum.map(strategy)
+    |> Enum.map(&(List.first(&1) * 10 + List.last(&1)))
     |> Enum.sum()
   end
 
-  def solve(file) do
+  def solve1(file) do
     File.read!(file)
-    |> String.split("\n", trim: true)
-    |> solve_puzzle()
+    |> solve_puzzle(&to_number/1)
+    |> IO.puts()
+  end
+
+  def solve2(file) do
+    File.read!(file)
+    |> solve_puzzle(&with_string_to_number/1)
     |> IO.puts()
   end
 end

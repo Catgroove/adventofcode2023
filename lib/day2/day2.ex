6ddbb91e85366e4fcfaef5310fc9579fab37dead
Martin Norberg <nobbehhh@gmail.com>
2023-12-17 18:16:37 +0100

    refactor

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -1,43 +1,49 @@
 defmodule Day2 do
-  def valid_game?({count, "red"}), do: count <= 12
-  def valid_game?({count, "green"}), do: count <= 13
-  def valid_game?({count, "blue"}), do: count <= 14
-
-  def create_game_key(string),
-    do: with([_, id] <- String.split(string, " "), do: String.to_integer(id))
-
-  def create_result(string) do
-    string
-    |> String.split(";")
-    |> Enum.map(fn s ->
-      String.split(s, ",")
-      |> Enum.map(fn s ->
-        s
-        |> String.trim()
-        |> String.split(" ")
-        |> to_tuple()
-      end)
-    end)
-    |> Enum.flat_map(& &1)
-  end
-
-  def to_tuple([int_str, color]), do: {String.to_integer(int_str), color}
-
-  def solve_puzzle(file) do
-    file
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn game ->
-      with [game, result] <- String.split(game, ":"),
-           do: {create_game_key(game), create_result(result)}
-    end)
-    |> Enum.filter(fn {_, result} -> Enum.all?(result, &valid_game?/1) end)
-    |> Enum.map(fn {game, _} -> game end)
-    |> Enum.sum()
-  end
-
   def solve(file) do
     File.read!(file)
     |> solve_puzzle()
     |> IO.inspect()
   end
+
+  def solve_puzzle(file) do
+    file
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_game/1)
+    |> Enum.filter(fn {_, result} -> Enum.all?(result, &valid_game?/1) end)
+    |> Enum.map(fn {game, _} -> game end)
+    |> Enum.sum()
+  end
+
+  def parse_game("Game " <> game) do
+    {id, result} = Integer.parse(game)
+    <<": ">> <> result = result
+
+    result =
+      result
+      |> String.split(";")
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&parse_result/1)
+      |> Enum.flat_map(& &1)
+
+    {id, result}
+  end
+
+  def parse_result(result) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&parse_color/1)
+  end
+
+  def parse_color(color) do
+    color
+    |> String.split(" ")
+    |> to_tuple()
+  end
+
+  def to_tuple([int_str, color]), do: {String.to_integer(int_str), color}
+
+  def valid_game?({count, "red"}), do: count <= 12
+  def valid_game?({count, "green"}), do: count <= 13
+  def valid_game?({count, "blue"}), do: count <= 14
 end

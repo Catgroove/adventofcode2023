ff8af31ddc78699bc2f04c3d7619a024ebe17983
Martin Norberg <nobbehhh@gmail.com>
2023-12-17 13:46:04 +0100

    Exercise 1, part 2

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,20 +1,51 @@
 defmodule Day1 do
-  def clean_non_numbers(string), do: string |> String.replace(~r/[^\d]/, "")
+  @regex ~r/(?=(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))/
+  @string_number_map %{
+    "1" => 1,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9
+  }
 
-  def get_first_and_last_character(string), do: String.first(string) <> String.last(string)
+  def map_to_numbers(string) do
+    Regex.scan(@regex, string)
+    |> Enum.flat_map(& &1)
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(fn
+      key when is_map_key(@string_number_map, key) -> @string_number_map[key]
+      key -> key
+    end)
+  end
 
   def solve_puzzle(string) do
-    string |> clean_non_numbers |> get_first_and_last_character |> Integer.parse()
+    string
+    |> Enum.map(fn c ->
+      case map_to_numbers(c) do
+        [] -> 0
+        list -> List.first(list) * 10 + List.last(list)
+      end
+    end)
+    |> Enum.sum()
   end
 
   def solve(file) do
     File.read!(file)
     |> String.split("\n", trim: true)
-    |> Enum.map(fn a -> solve_puzzle(a) end)
-    |> Enum.map(fn
-      {value, ""} -> value
-    end)
-    |> Enum.sum()
+    |> solve_puzzle()
     |> IO.puts()
   end
 end

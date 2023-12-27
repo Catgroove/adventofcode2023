5c52cbefa452d5f95b5df235475f97f5b6226156
Martin Norberg <nobbehhh@gmail.com>
2023-12-27 12:06:55 +0100

    Day 8

---
+++ lib/day8/day8.ex
@@ -0,0 +1,63 @@
+defmodule Day8 do
+  def solve(file) do
+    File.read!(file)
+    |> solve_puzzle()
+    |> IO.inspect()
+  end
+
+  def solve_puzzle(file) do
+    [instructions, nodes] =
+      file
+      |> String.split("\n\n")
+
+    nodes =
+      nodes
+      |> String.split("\n", trim: true)
+      |> Enum.map(&map_node/1)
+      |> Enum.reduce(%{}, &Map.merge/2)
+
+    #
+    # IO.inspect(map_instructions(instructions), label: "woof")
+    # IO.inspect(nodes, label: "nodes")
+
+    traverse(map_instructions(instructions), nodes)
+  end
+
+  def traverse(instructions, nodes, value \\ "AAA", acc \\ [], count \\ 0)
+
+  def traverse(_, _, "ZZZ", _, count), do: count
+
+  def traverse([], nodes, value, acc, count),
+    do: traverse(Enum.reverse(acc), nodes, value, [], count)
+
+  def traverse([instruction | instructions], nodes, value, acc, count),
+    do: traverse(instructions, nodes, nodes[value][instruction], [instruction | acc], count + 1)
+
+  def map_instructions(instructions) do
+    instructions
+    |> String.graphemes()
+    |> Enum.map(fn lr ->
+      case lr do
+        "R" -> :right
+        "L" -> :left
+      end
+    end)
+  end
+
+  def map_node(node) do
+    [head, lr] =
+      node
+      |> String.split(" = ")
+
+    lr =
+      lr
+      |> String.trim_leading("(")
+      |> String.trim_trailing(")")
+      |> String.split(", ")
+      |> map_lr()
+
+    %{head => lr}
+  end
+
+  def map_lr([left, right]), do: %{:left => left, :right => right}
+end
